8cdb9d92ada4df087ea417dc471533ac01b7526c
jonisukma22 <108139068+jonisukma22@users.noreply.github.com>
2022-06-25 19:31:17 +0700

    Add files via upload

---
+++ segitiga_form_view.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+ 		<title>Berikut Adalah Hasilnya</title>
+ 	</head>
+ 	<body>
+ 		<h2>Luas Segitiga</h2>
+ 		<form action="http://localhost/tugas_uas/ci/index.php/segitiga" method="post">
+ 			Masukkan nilai alas:<br/>
+ 			<input type="text" name="alas1">
+ 			<br>Masukkan nilai tinggi:<br/>
+ 			<input type="text" name="tinggi1"> <br/>
+ 			<input type="submit" name="btnSubmit" value="Hitung">
+ 		</form>
+ 	</body>
+</html>
